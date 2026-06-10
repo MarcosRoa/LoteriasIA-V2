e840da79cbad2a2e12a9a05147a03bd40a9a8e5c
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-06-10 16:32:55 -0300

    Create ping.js

---
+++ api/ping.js
@@ -0,0 +1,8 @@
+// api/ping.js
+export default function handler(req, res) {
+    res.status(200).json({ 
+        success: true, 
+        message: 'API funcionando!',
+        timestamp: new Date().toISOString()
+    });
+}
