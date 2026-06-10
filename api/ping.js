4f71f3ba567702719aaa38f0328e84c7fdd8d34b
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-06-10 18:22:23 -0300

    Delete api/ping.js

--- api/ping.js
+++
@@ -1,8 +0,0 @@
-// api/ping.js
-export default function handler(req, res) {
-    res.status(200).json({ 
-        success: true, 
-        message: 'API funcionando!',
-        timestamp: new Date().toISOString()
-    });
-}
