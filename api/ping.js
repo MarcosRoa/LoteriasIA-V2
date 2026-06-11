c2d7c560f427126726c1e6a4170b8b2639abeb39
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-06-11 16:38:51 -0300

    Update and rename ping-simple.js to ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,7 +1,6 @@
-// api/ping-simple.js
 export default function handler(req, res) {
-    res.status(200).json({
-        success: true,
+    res.status(200).json({ 
+        success: true, 
         message: 'Vercel está funcionando!',
         timestamp: new Date().toISOString()
     });
