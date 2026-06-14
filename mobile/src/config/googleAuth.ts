dd1d59f4db4959d424ca17ae303d7280bb07c5a9
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-06-14 17:35:28 -0300

    Update googleAuth.ts

--- mobile/src/config/googleAuth.ts
+++ mobile/src/config/googleAuth.ts
@@ -1,3 +1,4 @@
+// src/config/googleAuth.ts 14/06/2024
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
 import { makeRedirectUri } from 'expo-auth-session';
@@ -5,15 +6,14 @@ import { getAuth, signInWithCredential, GoogleAuthProvider } from 'firebase/auth
 
 WebBrowser.maybeCompleteAuthSession();
 
-// CLIENT ID DO ANDROID (que você criou)
 const ANDROID_CLIENT_ID = '124650527048-sl3c4r15v4fjtua8mdcj172at0chihe1.apps.googleusercontent.com';
-
-// CLIENT ID DO WEB (pode ser o antigo ou o novo - o que estiver no Firebase)
 const WEB_CLIENT_ID = '124650527048-sl3c4r15v4fjtua8mdcj172at0chihe1.apps.googleusercontent.com';
+const IOS_CLIENT_ID = '124650527048-sl3c4r15v4fjtua8mdcj172at0chihe1.apps.googleusercontent.com';
 
 export const useGoogleAuth = () => {
   const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
     webClientId: WEB_CLIENT_ID,
     redirectUri: makeRedirectUri({
       scheme: 'loteriasia',
@@ -30,8 +30,10 @@ export const useGoogleAuth = () => {
         const auth = getAuth();
         await signInWithCredential(auth, credential);
         return { success: true };
-      } else {
+      } else if (result?.type === 'cancel') {
         return { success: false, error: 'Login cancelado' };
+      } else {
+        return { success: false, error: 'Falha no login' };
       }
     } catch (error: any) {
       console.error('Erro no Google Login:', error);
