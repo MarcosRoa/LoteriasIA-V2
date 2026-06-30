8050b19cf64512b9ef2c6350d1577b5a405ba038
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-06-30 17:55:56 -0300

    Create lotomania.js

---
+++ public/js/estatisticas/loterias/lotomania.js
@@ -0,0 +1,13 @@
+// public/js/estatisticas/loterias/lotomania.js
+
+// ============================================
+// LOTOMANIA
+// ============================================
+
+import { renderizarBase } from '../renderers/base.js';
+
+export function renderizar(data, config, userData, periodo) {
+    return renderizarBase(data, config, userData, periodo);
+}
+
+export default { renderizar };
