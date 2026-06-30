d1a78c870dccea806687c3181f4d45bb111d8193
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-06-30 17:50:24 -0300

    Add renderizar function for Mega-Sena statistics

---
+++ public/js/estatisticas/loterias/megasena.js
@@ -0,0 +1,12 @@
+public/js/estatisticas/loterias/megasena.js
+//// ============================================
+// MEGA-SENA
+// ============================================
+
+import { renderizarBase } from '../renderers/base.js';
+
+export function renderizar(data, config, userData, periodo) {
+    return renderizarBase(data, config, userData, periodo);
+}
+
+export default { renderizar };
