c7d9222389a013eda63f95692406bca952bed449
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-07-21 16:53:37 -0300

    Update index.ts

--- api/generate/index.ts
+++ api/generate/index.ts
@@ -1,4 +1,4 @@
-import { RailwayClient } from '../../clients/RailwayClient';
+import { GenerateService } from '../../services/GenerateService';
 
 export default async function handler(req, res) {
     return res.json({
