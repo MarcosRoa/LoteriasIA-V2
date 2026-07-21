acf56369725f1b17a9f8c005b0d787f49b044bd4
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-07-21 16:06:49 -0300

    Update index.ts

--- api/generate/index.ts
+++ api/generate/index.ts
@@ -1,6 +1,7 @@
+import { GenerateService } from '../../services/GenerateService';
+
 export default async function handler(req, res) {
     return res.status(200).json({
-        teste: "INDEX V2",
-        timestamp: Date.now()
+        ok: true
     });
 }
