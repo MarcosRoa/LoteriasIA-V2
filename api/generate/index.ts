f1147557dd979b26177b4ecc3658b994a6f35854
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-07-21 17:59:08 -0300

    Update index.ts

--- api/generate/index.ts
+++ api/generate/index.ts
@@ -1,7 +1,21 @@
-import { CreditsService } from '../../services/CreditsService';
+import { GenerateService } from '../../services/GenerateService';
 
 export default async function handler(req, res) {
-    return res.json({
-        ok: true
-    });
+    try {
+        console.log("ANTES");
+
+        const service = new GenerateService();
+
+        console.log("DEPOIS");
+
+        return res.json({
+            ok: true
+        });
+    } catch (e) {
+        console.error("ERRO:", e);
+
+        return res.status(500).json({
+            error: e instanceof Error ? e.message : String(e)
+        });
+    }
 }
