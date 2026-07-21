e6ea9f08b26cfb823ea69e72993c3106fe105a88
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-07-21 16:10:00 -0300

    Update index.ts

--- api/generate/index.ts
+++ api/generate/index.ts
@@ -1,6 +1,13 @@
 import { GenerateService } from '../../services/GenerateService';
 
 export default async function handler(req, res) {
+
+    console.log("PASSO 1");
+
+    const service = new GenerateService();
+
+    console.log("PASSO 2");
+
     return res.status(200).json({
         ok: true
     });
