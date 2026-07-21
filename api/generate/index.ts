85ab12c7e411173e551c895801486cd078a3a639
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-07-21 16:51:33 -0300

    Update index.ts

--- api/generate/index.ts
+++ api/generate/index.ts
@@ -1,25 +1,7 @@
 import { RailwayClient } from '../../clients/RailwayClient';
 
 export default async function handler(req, res) {
-    try {
-        console.log('1');
-
-        const client = new RailwayClient();
-
-        console.log('2');
-
-        return res.json({
-            ok: true
-        });
-
-    } catch (e) {
-        console.error(e);
-
-        return res.status(500).json({
-            error: e instanceof Error ? {
-                message: e.message,
-                stack: e.stack
-            } : String(e)
-        });
-    }
+    return res.json({
+        ok: true
+    });
 }
