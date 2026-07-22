d9ea4f04dfb9e1c14bfd4f2d7808c1d2ae70889c
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-07-21 21:32:37 -0300

    Add API handler for test import

--- api/test-import.js
+++ api/test-import.js
@@ -1,1 +1,7 @@
 import { GenerateService } from '../../services/GenerateService';
+export default function handler(req, res) {
+    return res.status(200).json({
+        ok: true,
+        importou: 'RailwayClient'
+    });
+}
