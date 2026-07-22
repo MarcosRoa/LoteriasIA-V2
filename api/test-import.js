3b0150a3eb71b16896290c3f2ada6f0143803860
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-07-21 21:37:01 -0300

    Remove unused import and add Node version to response

--- api/test-import.js
+++ api/test-import.js
@@ -1,7 +1,6 @@
-import { GenerateService } from '../../services/GenerateService';
 export default function handler(req, res) {
     return res.status(200).json({
         ok: true,
-        importou: 'RailwayClient'
+        node: process.version
     });
 }
