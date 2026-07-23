41b8651cc0774b05899373dcef4d36c397a62099
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-07-23 16:33:01 -0300

    Add X-User-Id header to API request

--- public/js/jogos.js
+++ public/js/jogos.js
@@ -93,7 +93,7 @@ async function gerarJogos() {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${await window.apiClient.getFirebaseToken()}`,
-                'X-User-Id': window.usuarioAtual?.uid  // ← ADICIONAR ESTA LINHA
+                'X-User-Id': window.usuarioAtual?.uid
             },
             body: JSON.stringify({
                 lotteryType: loteria,
